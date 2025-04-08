2866e954d0dd073f1d748aac0092a90a2d10fb1b
lukashartnett <135441677+lukashartnett@users.noreply.github.com>
2025-04-08 12:12:17 +0000

    saving

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ function setup() {
     setupDebugConsole();
 }
 
-function draw(){
-
+function draw() {
+    background(255); 
+    textSize(32); // Set the text size
+    textAlign(CENTER, CENTER); 
+    fill(0); 
+    text("Hello World", width / 2, height / 2); 
 }
